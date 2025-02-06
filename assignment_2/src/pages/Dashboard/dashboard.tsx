a7ed86f24ce7fd8c6b3ad82c3686a06c7e2d345c
Chaoyi Wu <chaoyiwu20@gmail.com>
2025-02-06 12:59:53 -0500

    current user display

--- assignment_2/src/pages/Dashboard/dashboard.tsx
+++ assignment_2/src/pages/Dashboard/dashboard.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect, useCallback, Dispatch, SetStateAction } from 'react'
 import { Button, Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
+import { User } from 'firebase/auth'
 import { firebaseSignOut, firebaseGetAllUsers } from '../../firebase/FirebaseService'
+import { useAuth } from '../../contexts/useAuth'
 
 const handleSignOut = () => {
     SignOut()
@@ -12,13 +14,16 @@ const SignOut = async () => {
     catch (error) {}
 }
 
+
 const Dashboard = () => {
     const [allUsers, setAllUsers] = useState<string[]>([])
-    const [loading, setLoading] = useState(true)
-
+    const auth = useAuth()
+    const [currentUser, setCurrentUser] = useState<User | null>(auth.user)
+    
     useEffect(() => {
         let ignore = false
         try {
+            setCurrentUser(auth.user)
             console.log("attempting to load all users from firebase")
             firebaseGetAllUsers()
             .then((data)=> {
@@ -30,34 +35,43 @@ const Dashboard = () => {
                 ignore = true
             }
         }catch(error){
-            console.error("error fetching a list of all users")
+            console.error(error)
         }
-    },  [allUsers])
+    },  [])
 
     return (
-        <div>
-            <TabGroup vertical>
-                <TabList className="flex flex-col">
-                    <Tab className="data-[selected]:bg-blue-500 data-[selected]:text-white data-[hover]:underline">Widgets</Tab>
-                    <Tab className="data-[selected]:bg-blue-500 data-[selected]:text-white data-[hover]:underline">User Profile</Tab>
-                    <Tab className="data-[selected]:bg-blue-500 data-[selected]:text-white data-[hover]:underline">All Users</Tab>
+        <div className='flex h-screen w-full justify-center pt-24 px-4'>
+            <div className='w-full max-w-md'>
+            <TabGroup>
+                <TabList className="flex flex-gap4">
+                    <Tab className="data-[selected]:bg-blue-500 data-[hover]:underline">Widgets</Tab>
+                    <Tab className="data-[selected]:bg-blue-500 data-[hover]:underline">User Profile</Tab>
+                    <Tab className="data-[selected]:bg-blue-500 data-[hover]:underline">All Users</Tab>
                     </TabList>
-                    <TabPanels>
-                        <TabPanel>Content 1</TabPanel>
-                        <TabPanel>Content 2</TabPanel>
-                        <TabPanel>
-                            <ul>
-                            {allUsers.map((user: string, index: number) => (
-                            <li key={index}>
-                                {user}
-                            </li>
+                    <TabPanels className="mt-3">
+                        <TabPanel className="rounded-xl bg-white/5 p-3">
+                            Content 1
+                            </TabPanel>
+                        <TabPanel className="rounded-xl bg-white/5 p-3">
+                            <p style={{ textAlign: 'left' }}>Name: {currentUser?.displayName}</p>
+                            <p style={{ textAlign: 'left' }}>Email: {currentUser?.email}</p>
+                            <p style={{ textAlign: 'left' }}>Created At: {currentUser?.metadata.creationTime}</p>
+                            <p style={{ textAlign: 'left' }}>Last Signed In: {currentUser?.metadata.lastSignInTime}</p>
+                        </TabPanel>
+                        <TabPanel className="rounded-xl bg-white/5 p-3">
+                            <ul className="gap-2 text-white/50">
+                                {allUsers.map((user: string, index: number) => (
+                                <li key={index}
+                                    className='relative rounded-md p-3 text-sm/6 transition hover:bg-white/5'>
+                                    {user}
+                                </li>
                                 ))}
                             </ul>
                         </TabPanel>
                     </TabPanels>
                 </TabGroup>
             <Button onClick={handleSignOut}>Sign Out</Button>
-
+            </div>
         </div>
     );
 };
